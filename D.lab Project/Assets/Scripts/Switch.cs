e3b7c5b73db8cfccf16c29ff90501da64524983e
jzara3115 <jzara3115@gmail.com>
2021-12-02 13:28:07 -0800

    switch stuff

--- D.lab Project/Assets/Scripts/Switch.cs
+++ D.lab Project/Assets/Scripts/Switch.cs
@@ -6,17 +6,33 @@ public class Switch : MonoBehaviour
 {
     public List<GameObject> ObjectsToDestroy;
     private int currentActiveIndex = 0;
+    private bool on = false;
+    private AudioSource ss;
+    [SerializeField]
+    private AudioClip clip;
 
-    void OnTriggerStay(Collider col){
-        if(col.gameObject.CompareTag("Player")){
-        if(Input.GetKey(KeyCode.E)){
+    void Awake(){
+        ss = GetComponent<AudioSource>();
+    }
+
+    void Update(){
+        if (on==true){
+        ss.PlayOneShot(clip);
         ObjectsToDestroy[currentActiveIndex].SetActive(false);
         currentActiveIndex++;
     if (currentActiveIndex >= ObjectsToDestroy.Count)
         currentActiveIndex = 0;
         }
-        }
     }
+
+    void OnTriggerStay(Collider col){
+        if(col.gameObject.CompareTag("Player") && on == false){
+        if(Input.GetKey(KeyCode.E)){
+        on = true;
+    }
+    }
+    }
+
 
 }
 
