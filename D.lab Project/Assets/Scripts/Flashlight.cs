b0fb6888702fda50d155909bafa9fcea061fd4d4
jzara3115 <jzara3115@gmail.com>
2021-12-06 12:22:01 -0800

    flashlight turn on
    :100:

--- D.lab Project/Assets/Scripts/Flashlight.cs
+++ D.lab Project/Assets/Scripts/Flashlight.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class Flashlight : MonoBehaviour
 {
     
-    public Transform playerface;
+    public GameObject playerface;
     private SphereCollider sc;
     public GameObject Light;
+    public bool collected = false;
+    public bool on = true;
 
     void Start()
     {
@@ -16,17 +18,30 @@ public class Flashlight : MonoBehaviour
 
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.E)&&collected == true){
+            if(on){
+                Light.SetActive(false);
+                on = false;
+            } else{
+                Light.SetActive(true);
+                on = true;
+            }
     }
+
+}
 
     void OnTriggerStay(Collider col){
     if(col.gameObject.CompareTag("Player")){
     		if(Input.GetKey(KeyCode.E)){
             sc.enabled = !sc.enabled;
-
+            collected = true;
+            gameObject.transform.parent = playerface.transform;
+            gameObject.transform.position = playerface.transform.position;
+            gameObject.transform.rotation = playerface.transform.rotation;
             Debug.Log("Flashlight");
         }
     	}
     }
+
 
 }
